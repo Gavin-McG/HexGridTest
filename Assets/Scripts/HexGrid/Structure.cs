2036cd09ba0f2b583e645fd3f07b3a3eeffba11b
Gavin-McG <gavinmcg@umich.edu>
2024-10-09 11:09:35 -0400

    structure script
    Structure is a scriptableObject which defines the tiles and tile positions for types of buildings. Necessary if we intend to do multi-tile structures

---
+++ Assets/Scripts/HexGrid/Structure.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Timeline.Actions;
+using UnityEngine;
+
+//defines the structure of tiles for a type of building
+
+[System.Serializable]
+public struct StructurePeice
+{
+    public CustomTile tile;
+    public Vector3Int cubicCoord;
+}
+
+[CreateAssetMenu(fileName = "Structure", menuName = "ScriptableObjects/Structure", order = 1)]
+public class Structure : ScriptableObject
+{
+    public StructurePeice[] peices;
+}
