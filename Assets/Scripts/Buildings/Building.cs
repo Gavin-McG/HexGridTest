3e5d0e2523fc7795ea5dfd13a34ea01b64f5c0ee
Gavin-McG <gavinmcg@umich.edu>
2024-10-17 13:42:10 -0400

    Upgrade struct
    Struct to store data required for upgrades in Buildings.

--- Assets/Scripts/Buildings/Building.cs
+++ Assets/Scripts/Buildings/Building.cs
@@ -7,10 +7,20 @@ public enum BuildingType
     MainTower,
     Building0
 }
+
+[System.Serializable]
+public struct Upgrade
+{
+    public Structure newStructure;
+    public Resources upgradeCost;
+}
+
 public abstract class Building : MonoBehaviour
 {
     public abstract string buildingName { get; }
     public abstract BuildingType type { get; }
 
     public Resources buildCost;
+    public int level = 0;
+    public Upgrade[] upgrades = new Upgrade[0];
 }
