a759e8ae1d9df5a4949779a4c405af900bbc56b1
Gavin-McG <gavinmcg@umich.edu>
2024-10-17 11:49:51 -0400

    Resource Class
    Add class Resources to store resources and include relevant operators

---
+++ Assets/Scripts/Resources/ResourceManager.cs
@@ -0,0 +1,13 @@
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ResourceManager : MonoBehaviour
+{
+    public Resources currentResource;
+
+    
+}
