280714eca81bc37125d761f68be43e87e25eaca1
Gavin-McG <gavinmcg@umich.edu>
2024-10-31 13:36:21 -0400

    FighterPanel script
    script for future dungeonUI to interact with fighterpanel

---
+++ Assets/Scripts/UI/Dungeon/FighterPanel.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FighterPanel : MonoBehaviour
+{
+    [SerializeField] Image headImage;
+    [SerializeField] TextMeshProUGUI nameText;
+    [SerializeField] Image classImage;
+    [SerializeField] TextMeshProUGUI classText;
+    [SerializeField] RectTransform skillBackground;
+    [SerializeField] RectTransform skillBar;
+    [SerializeField] RectTransform strengthBar;
+    [SerializeField] RectTransform teamworkBar;
+    [SerializeField] RectTransform healthBackground;
+    [SerializeField] RectTransform healthBar;
+    [SerializeField] GameObject deadPanel;
+    [SerializeField] GameObject readyPanel;
+    [SerializeField] GameObject notReadyPanel;
+
+    public void SetSkills(Skills skill)
+    {
+        SetSkill(skillBar, skill.skill);
+        SetSkill(strengthBar, skill.strength);
+        SetSkill(teamworkBar, skill.teamwork);
+    }
+
+    void SetSkill(RectTransform bar, float stat)
+    {
+        bar.sizeDelta = new Vector2(skillBackground.sizeDelta.x, stat * skillBackground.sizeDelta.y);
+    }
+
+    public void SetHealth(float health)
+    {
+        healthBar.sizeDelta = new Vector2(skillBackground.sizeDelta.x, health * skillBackground.sizeDelta.y);
+    }
+
+    public void SetHead(Sprite sprite)
+    {
+        headImage.sprite = sprite;
+    }
+
+    public void SetName(string name)
+    {
+        nameText.text = name;
+    }
+
+    public void SetClass(string className, Color classColor)
+    {
+        classImage.color = classColor;
+        classText.text = className;
+    }
+
+    public void SetDead(bool active)
+    {
+        deadPanel.SetActive(active);
+    }
+
+    public void SetReady()
+    {
+        readyPanel.SetActive(true);
+        notReadyPanel.SetActive(false);
+    }
+
+    public void SetNotReady()
+    {
+        readyPanel.SetActive(false);
+        notReadyPanel.SetActive(true);
+    }
+
+    public void clearPanel()
+    {
+        readyPanel.SetActive(false);
+        notReadyPanel.SetActive(false);
+    }
+}
