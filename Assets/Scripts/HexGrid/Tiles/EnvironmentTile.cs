bd4c83a635514407904c3f22caf06f6732830814
cspears8 <connorxspears@gmail.com>
2024-11-07 18:05:52 -0500

    Wood Production Work

--- Assets/Scripts/HexGrid/Tiles/EnvironmentTile.cs
+++ Assets/Scripts/HexGrid/Tiles/EnvironmentTile.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -14,4 +15,25 @@ public class EnvironmentTile : BasicTile
     [Space(10)]
     [SerializeField] EnvironmentType envType;
     [SerializeField] public string tileName;
+    [SerializeField] private ResourceManager rm;
+
+    void Awake()
+    {
+        BuildingManager.EnvironmentDeleted.AddListener(OnDestroyTile);
+    }
+
+    void OnDestroyTile(EnvironmentTile environmentTile, Vector3Int position)
+    {
+        switch (environmentTile.envType)
+        {
+            case EnvironmentType.Tree:
+                //Set refund rate to 1 to ensure that you get the same amount of wood every time
+                int woodAmount = (int)Math.Round(20 / rm.refundRate);
+                rm.Refund(new Resources(0, woodAmount, 0, 0));
+                break;
+            default:
+                Debug.LogError("envType: " + environmentTile.envType + " does not exist!");
+                break;
+        }
+    }
 }
