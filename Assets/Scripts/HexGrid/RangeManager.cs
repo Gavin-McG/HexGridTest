85dff5c876e50dee8a402303667df32a3a738e5f
Gavin-McG <gavinmcg@umich.edu>
2024-11-15 16:08:36 -0500

    Web Build fixes
    Fixes and changes required to create web build

--- Assets/Scripts/HexGrid/RangeManager.cs
+++ Assets/Scripts/HexGrid/RangeManager.cs
@@ -1,10 +1,7 @@
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
-using UnityEngine.Rendering;
 using UnityEngine.Tilemaps;
-using UnityEngine.WSA;
 
 public class RangeManager : MonoBehaviour
 {
